4dd9dad0be35b98ef73217e4ec5c6543e135a497
Natallia Kiryshun <namolosh@gmail.com>
2024-06-11 21:22:51 +0200

    Implement basic routing

--- src/app/overlay-window/overlay-window.component.ts
+++ src/app/overlay-window/overlay-window.component.ts
@@ -1,9 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'aiq-overlay-window',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './overlay-window.component.html',
   styleUrl: './overlay-window.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
---
+++ src/app/training/training/training.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-training',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './training.component.html',
+  styleUrl: './training.component.css'
+})
+export class TrainingComponent {
+
+}
